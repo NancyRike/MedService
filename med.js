ec6832166d025613a1e448159f8cddbd4cb77117
NancyRike <nancyrike7@gmail.com>
2020-03-03 21:43:43 +0100

    js_form_value

--- med.js
+++ med.js
@@ -8,6 +8,20 @@ function toggle_form(){
     else{
         toToggle.style.display = "none"
     }
-    
+}
 
+var submit = document.getElementById("submit")
+document.addEventListener("submit" , toSubmit);
+function toSubmit(e) {
+    e.preventDefault()
+var pName = document.getElementById("pName").value;
+var pGender = document.getElementById("pGender").value;
+var pAge = document.getElementById("pAge").value
+var pSpecs = document.getElementById("pSpecs").value;
+var pNumber = document.getElementById("pNumber").value;
+var pSymptom = document.getElementById("pSymptom").value;
+
+ document.getElementById("showAppoint").innerText=`Dear ${pName} 
+ you  will see a consultant in ${pSpecs} please head to the ${pGender} 
+ consulting room by the left, a nurse awaits you to confirm the reason for your ${pSymptom}`
 }
